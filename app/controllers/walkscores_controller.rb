b75c83d68f20ff21bcb205d71f97b82487ac571b
Graham Turner <turnertgraham@gmail.com>
2016-08-24 17:01:41 -0400

    finished walkscore PORO

--- app/controllers/walkscores_controller.rb
+++ app/controllers/walkscores_controller.rb
@@ -1,2 +1,7 @@
 class WalkscoresController < ApplicationController
+
+  def show
+    @response = WalkscoreMain.get_transitscore("3404 Harden Road Raleigh NC 27607")
+  end
+
 end
--- app/models/walkscore_main.rb
+++ app/models/walkscore_main.rb
@@ -1,27 +1,74 @@
 class WalkscoreMain
 
+#HTTParty.get("http://transit.walkscore.com/transit/score/?lat=47.6101359&lon=-122.3420567&city=Seattle&state=WA&wsapikey=4c1811146683d4a9656db9224300d8d3")
+
+
+  def self.get_transitscore(address)
+    url = build_url(address, "transit")
+    HTTParty.get(url)
+  end
+
   def self.get_walkscore(address)
-    url = build_url(address)
-    #response = HTTParty.get(url)
-    #p response
+    url = build_url(address, "walk")
+    HTTParty.get(url)
   end
 
-  def self.build_url(address)
-    url = "http://api.walkscore.com/score?format=json"
-    url_hash = build_url_hash(address)
-    url_hash.each do |key, val|
-      url += "&#{key}=#{val}"
+  private
+
+    # Takes the type of call, "walk" or "transit", generates the begginning of a
+    # url string and builds a hash of all the keys and values that string needs
+    # to be built. Returns the final string
+    def self.build_url(address, type)
+      if type == "walk"
+        url_hash = build_url_hash_walk(address)
+        url_beginning = "http://api.walkscore.com/score?format=json"
+      else
+        url_hash = build_url_hash_transit(address)
+        url_beginning = "http://transit.walkscore.com/transit/score/?"
+      end
+      build_url_string(url_beginning, url_hash)
     end
-    url
-  end
 
-  def self.build_url_hash(address)
-    hash = {}
-    hash["address"] = URI.encode(address)
-    geocoded = Geokit::Geocoders::GoogleGeocoder.geocode address
-    hash["lat"], hash["lon"] = geocoded.latitude, geocoded.longitude
-    hash["wsapikey"] = Rails.application.secrets.walkscore_key
-    hash
-  end
+    # given a starting url_string and a hash of attributes, builds a final url
+    # string to be submitted to the walkscore API. Returns a completed url string.
+    def self.build_url_string(url, url_hash)
+      url_hash.each do |key, val|
+        if key == "city"
+          url += "#{key}=#{val}"
+        else
+          url += "&#{key}=#{val}"
+        end
+      end
+      url
+    end
+
+    def self.build_url_hash_transit(address)
+      hash = get_city_state(address)
+      hash = get_lat_long(address, hash)
+      hash["wsapikey"] = Rails.application.secrets.walkscore_key
+      hash
+    end
+
+    def self.get_city_state(address)
+      hash = {}
+      address_params = address.split(" ")
+      hash["city"], hash["state"] = address_params[-3], address_params[-2]
+      hash
+    end
+
+    def self.get_lat_long(address, hash)
+      geocoded = Geokit::Geocoders::GoogleGeocoder.geocode address
+      hash["lat"], hash["lon"] = geocoded.latitude, geocoded.longitude
+      hash
+    end
+
+
+    def self.build_url_hash_walk(address)
+      hash = {}
+      hash["address"] = URI.encode(address)
+      hash = get_lat_long(address, hash)
+      hash["wsapikey"] = Rails.application.secrets.walkscore_key
+      hash
+    end
 
 end
