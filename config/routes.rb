8d0c6805afbddcd16f75bc3ef5d7554e594de7e4
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-08-24 14:39:15 -0700

    Changes user parameters

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  
-  devise_scope :user do
-    root to: 'devise/registrations#new'
-  end
-  
+  root to: 'users#show'
+
+  # devise_scope :user do
+  #   root to: 'devise/registrations#new'
+  # end
+
 end
