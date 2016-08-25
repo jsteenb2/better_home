0fc5b79f6dd7cdaecdc5f4cfacdd22ff58628e40
cjvirtucio87 <cjv287@gmail.com>
2016-08-24 17:09:37 -0700

    hotfix

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,11 +4,6 @@ class SearchesController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
-=======
-<<<<<<< 0e3c48fd1fb31ba8252f0558664804b29cb06aa9
-<<<<<<< dcf57c01bf33a401f5d639013cb9dff920dcdc82
->>>>>>> from-remote
   end
 
   def show
@@ -19,14 +14,6 @@ class SearchesController < ApplicationController
   def index
     prep_region_children
     @neighborhoods = get_region_children
-<<<<<<< HEAD
-=======
-=======
-    prep_region_children
-    @neighborhoods = get_region_children
-    render :index
-=======
->>>>>>> Basic view.
   end
 
   def show
@@ -35,29 +22,12 @@ class SearchesController < ApplicationController
   end
 
   def index
-<<<<<<< 0e3c48fd1fb31ba8252f0558664804b29cb06aa9
->>>>>>> Added view for search results
-=======
     prep_region_children
     @neighborhoods = get_region_children
->>>>>>> Basic view.
->>>>>>> from-remote
   end
 
   private
-
-<<<<<<< HEAD
     # Getting neighborhoods.
-=======
-<<<<<<< 0e3c48fd1fb31ba8252f0558664804b29cb06aa9
-<<<<<<< dcf57c01bf33a401f5d639013cb9dff920dcdc82
-    # Getting neighborhoods.
-=======
->>>>>>> Added view for search results
-=======
-    # Getting neighborhoods.
->>>>>>> Basic view.
->>>>>>> from-remote
     def prep_region_children
       @client = ZillowGetRegionChildren.new
       params.except(:utf8,:authenticity_token,:controller,:action,:commit).each do |k,v|
@@ -74,13 +44,6 @@ class SearchesController < ApplicationController
       @client.search
       @client.parsed_results
     end
-<<<<<<< HEAD
-=======
-<<<<<<< 0e3c48fd1fb31ba8252f0558664804b29cb06aa9
-<<<<<<< dcf57c01bf33a401f5d639013cb9dff920dcdc82
-=======
->>>>>>> Basic view.
->>>>>>> from-remote
 
     # Getting a property.
     def prep_deep_search
@@ -98,12 +61,3 @@ class SearchesController < ApplicationController
       @client.search
       @client.parsed_results
     end
-<<<<<<< HEAD
-=======
-<<<<<<< 0e3c48fd1fb31ba8252f0558664804b29cb06aa9
-=======
->>>>>>> Added view for search results
-=======
->>>>>>> Basic view.
->>>>>>> from-remote
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824174422) do
+ActiveRecord::Schema.define(version: 20160824201624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,15 +40,17 @@ ActiveRecord::Schema.define(version: 20160824174422) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",          null: false
+    t.string   "name",                                null: false
     t.integer  "age"
-    t.string   "email",         null: false
+    t.string   "email",                               null: false
     t.integer  "location_id"
     t.integer  "walk_score"
     t.integer  "transit_score"
     t.integer  "commute_score"
     t.integer  "cost_score"
     t.integer  "crime_score"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -71,9 +73,6 @@ ActiveRecord::Schema.define(version: 20160824174422) do
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["email"], name: "index_users_on_email", using: :btree
   end
 
   create_table "zips", force: :cascade do |t|
