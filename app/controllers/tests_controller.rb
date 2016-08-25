14cfb24547d200bc1aad1773a6de61fae725a76a
Dylan Lynch <lynchd2@tcnj.edu>
2016-08-25 15:39:56 -0400

    Scores for crime, fire safety, fire incidents, evictions

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,17 +1,12 @@
 class TestsController < ApplicationController
   def new
     @addressinformation = AddressInformation.new
-    @eviction_notices_neighborhood = @addressinformation.eviction_notices_in("Tenderloin", 2016)
-    @total_evictions_year = @addressinformation.total_eviction_notices
-    @firesafety_complaints_year = @addressinformation.firesafety_complaints_in("Tenderloin", 2016)
-    @neighborhood_firesafety_complaints = @addressinformation.total_firesafety_complaints
     @total_neighborhoods = @addressinformation.total_neighborhoods
-    @crime_in_neighborhood = @addressinformation.crime_in_neighborhood("Tenderloin", 2016)
-    @total_crime = @addressinformation.total_crime
-    @fire_incidents_in = @addressinformation.fire_incidents_in("Tenderloin", 2016)
-    @total_fire_incidents = @addressinformation.total_fire_incidents
-    #@traffic_incidents_in = @addressinformation.traffic_incidents_in("Tenderloin", 2016)
-    @total_traffic_incidents = @addressinformation.total_traffic_incidents
-
+    #@traffic_incidents_in = @addressinformation.traffic_incidents_in("Tenderloin", 2015)
+    #@total_traffic_incidents = @addressinformation.total_traffic_incidents
+    @eviction_score = @addressinformation.eviction_score("Mission", 2015)
+    @fire_safety_score = @addressinformation.fire_safety_score("Mission", 2015)
+    @crime_score = @addressinformation.crime_score("Mission", 2015)
+    @fire_incidents_score = @addressinformation.fire_incidents_score("Mission", 2015)
   end
 end
